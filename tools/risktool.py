fcba4b8a56c7f17f71171e2c1d146985acd26b88
Nicolai Norseng <Nicolai.Norseng@indra.no>
2021-01-05 13:04:58 +0100

    fixed crash if not entering values

--- tools/risktool.py
+++ tools/risktool.py
@@ -5,23 +5,28 @@ risk_trade = float("{:.3f}".format(risk_trade))
 print("risk per trade: ", risk_trade)
 
 while True:
-    entry = input("enter entry price: ")
-    stop = input("enter stoploss: ")
-    pt = input("enter profit target: ")
-    risk_share=float(entry) - float(stop)
-    risk_share=float("{:.3f}".format(risk_share))
+    print("**************************************")
+    try:
+        entry = input("enter entry price: ")
+        stop = input("enter stoploss: ")
+        pt = input("enter profit target: ")
+        risk_share=float(entry) - float(stop)
+        risk_share=float("{:.3f}".format(risk_share))
 
 
 
-    print("risk per share is:", risk_share)
-    sharesize=int(risk_trade/risk_share)
-    print("sharesize is: ", sharesize)
-    potential_profitt=(float(pt) - float(entry))*sharesize
-    potential_profitt = float("{:.3f}".format(potential_profitt))
-    print("potential profitt is:", potential_profitt)
-    risk_rew=int(potential_profitt/risk_trade)
-    print("risk reward for this trade: ", risk_rew)
-    print("\n")
+        print("risk per share is:", risk_share)
+        sharesize=int(risk_trade/risk_share)
+        print("sharesize is: ", sharesize)
+        potential_profitt=(float(pt) - float(entry))*sharesize
+        potential_profitt = float("{:.3f}".format(potential_profitt))
+        print("potential profitt is:", potential_profitt)
+        risk_rew=int(potential_profitt/risk_trade)
+        print("risk reward for this trade: ", risk_rew)
+        print("\n")
+    except:
+        print("******* no values entered..try again ********")
+        continue
 
 
 
